a1d3eb271d9136b09b27f8cc6791966e040f1977
viniciuschiele <vinicius.chiele@gmail.com>
2016-11-23 18:21:48 +0000

    Remove .h files

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 find ./contracts -type f -name '*.so' -delete
 find ./contracts -type f -name '*.c' -delete
+find ./contracts -type f -name '*.h' -delete
 
 python setup.py build_ext --inplace --cython
--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 find ./contracts -type f -name '*.so' -delete
 find ./contracts -type f -name '*.c' -delete
+find ./contracts -type f -name '*.h' -delete
